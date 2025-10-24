be69b014331591bf4a6f370f0a70d357e700a3fd
rip-on <shipon.ripon123@gmail.com>
2025-10-24 14:36:32 +0600

    small update

--- new-app/src/Color.jsx
+++ new-app/src/Color.jsx
@@ -7,15 +7,13 @@ function Color(){
          setColor(e.target.value)
     }
     return(
-        <div>
+        <div className="color-container">
                 <h1>Color Picker</h1>
-                <div className="color-diplay" style={{backgroundColor: color}}></div>
+                <div className="color-display" style={{backgroundColor: color}}>
                 <p>Selected Color: {color}</p>
-        
+                </div>
 
-            <label htmlFor="">
-            Select a color:
-            </label>
+            <label htmlFor="">Select a color:</label>
             <input type="color" value={color} onChange={changeColor} />
         </div>
     )
